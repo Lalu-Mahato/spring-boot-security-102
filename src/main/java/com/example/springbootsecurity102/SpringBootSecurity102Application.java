2edd45e20dd6782229db73479b26899ee274aa1d
Lalu-Mahato <135944020+Lalu-Mahato@users.noreply.github.com>
2023-09-15 15:30:34 +0530

    first commit

---
+++ src/main/java/com/example/springbootsecurity102/SpringBootSecurity102Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootsecurity102;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurity102Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurity102Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootsecurity102/SpringBootSecurity102ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootsecurity102;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurity102ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
